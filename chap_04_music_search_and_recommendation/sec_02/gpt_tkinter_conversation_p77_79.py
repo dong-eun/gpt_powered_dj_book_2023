8a6a8588ad08299384680f3f61219e54928d51d9
dominic <rednman@gmail.com>
2024-02-12 11:24:42 +0900

    appkey 암호화

--- chap_04_music_search_and_recommendation/sec_02/gpt_tkinter_conversation_p77_79.py
+++ chap_04_music_search_and_recommendation/sec_02/gpt_tkinter_conversation_p77_79.py
@@ -1,10 +1,12 @@
 import openai
 import tkinter as tk
 from tkinter import scrolledtext
+from dotenv import load_dotenv
+import os
 
-# openai.api_key = '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'
-from api_keys import openai_api_key # API key가 github에 올라가면 폐기되기 때문에 따로 import 했습니다.
-openai.api_key=openai_api_key  # API key가 github에 올라가면 폐기되기 때문에 따로 import 했습니다.
+load_dotenv()
+
+openai.api_key=os.getenv("apiKey")
 
 def send_message(message_log):
     response=openai.ChatCompletion.create(
